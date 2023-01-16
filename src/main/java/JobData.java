74adb15f00db5a3868594b215239c0f1d4cb5231
Ballarja <JamesBallard87@hotmail.com>
2023-01-16 15:30:23 -0600

    findByValue method updated added nested for each loops with if statement

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -5,10 +5,7 @@ import org.apache.commons.csv.CSVRecord;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by LaunchCode
@@ -99,7 +96,16 @@ public class JobData {
         loadData();
 
         // TODO - implement this method
-        return null;
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+
+        for (HashMap<String, String> row : allJobs) {
+            for (String element : row.values()){
+                if(element.contains(value)){
+                    jobs.add(row);
+                }
+            }
+        }
+        return jobs;
     }
 
     /**
--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Created by LaunchCode
@@ -61,7 +58,8 @@ public class TechJobs {
                 System.out.println("\nSearch term:");
                 String searchTerm = in.nextLine();
 
-                if (searchField.equals("all")) {
+                if (searchField.equalsIgnoreCase("all")) {
+
                     printJobs(JobData.findByValue(searchTerm));
                 } else {
                     printJobs(JobData.findByColumnAndValue(searchField, searchTerm));
@@ -123,12 +121,12 @@ public class TechJobs {
         if(!someJobs.isEmpty()) {
      // Outer loop iterates through the ArrayList of HashMap objects
             for (HashMap<String, String> jobSearch : someJobs) {
-                System.out.println("*****");
+                System.out.println("\n*****");
      // Inner loop iterates through HashMap key value pairs using entrySet
                 for (Map.Entry<String, String> jobInfo : jobSearch.entrySet()) {
                     System.out.println(jobInfo.getKey() + ": " + jobInfo.getValue());
                 }
-                System.out.println("*****\n");
+                System.out.println("*****");
             }
         }
         else{
